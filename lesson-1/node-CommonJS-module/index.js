21c284ea9a16238f3d4226edf192485ff714928f
unknown <bogdan.lyamzin.d@gmail.com>
2023-01-25 21:23:47 +0200

    finish lesson 1

---
+++ lesson-1/node-CommonJS-module/index.js
@@ -0,0 +1,36 @@
+// CommonJS
+// const nodemon = require("nodemon");
+
+const obj = require("./users");
+// console.log(obj);
+
+const {admins} = require("./users");
+// console.log(admins);
+
+// const {getCurrentMonth} = require("./date");
+// const currentMonth = getCurrentMonth();
+// console.log(currentMonth);
+
+const currentMonth = require("./date").getCurrentMonth();
+console.log(currentMonth);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lesson-1/node-module-es6-mjs/index.mjs
@@ -0,0 +1,25 @@
+import users from "./users.mjs";
+import {getCurrentMonth} from "./date/index.mjs";
+
+console.log(users);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
