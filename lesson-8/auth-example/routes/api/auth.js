4a0e5442799dbb54bfcccecee0dcca0fc6c58305
unknown <bogdan.lyamzin.d@gmail.com>
2023-02-15 18:33:00 +0200

    fix

---
+++ lesson-8/auth-example/routes/api/auth.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const ctrl = require("../../controllers/auth")
+
+const {authenticate, validateBody} = require("../../middlewares");
+
+const {schemas} = require("../../models/user");
+
+const router = express.Router();
+
+// signup
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+
+router.get("/current", authenticate, ctrl.getCurrent);
+
+router.post("/logout", authenticate, ctrl.logout);
+
+module.exports = router;
